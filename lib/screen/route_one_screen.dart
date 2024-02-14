1021fd81b64843fce19df3bdb7fcd4d03d480085
iammoong <ansrlwkd225@naver.com>
2024-02-15 00:14:06 +0900

    fail

---
+++ lib/screen/route_one_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class RouteOneScreen extends StatelessWidget {
+  const RouteOneScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HomeScreen'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              'Push',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
